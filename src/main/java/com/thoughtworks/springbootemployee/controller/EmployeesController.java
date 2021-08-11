641381c5a5c8c2fd70aaad633f4effdfcb4ce685
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-11 16:26:52 +0800

    [employeesPairProgramming][Juan Carlos Salvador] refactor: change method name from getAllMaleEmployees to getAllGenderEmployees and formatted lines

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -13,12 +13,12 @@ public class EmployeesController {
     private final List<Employee> employees = new ArrayList<>();
 
     public EmployeesController() {
-        employees.add(new Employee(1,"Yuto",23,"Male",15000));
-        employees.add(new Employee(2,"JC",22,"Male",10000));
-        employees.add(new Employee(3,"Khali",21,"Female",10000));
-        employees.add(new Employee(4,"Donald",21,"Male",104400));
-        employees.add(new Employee(5,"Bob",69,"Male",10000));
-        employees.add(new Employee(6,"Zagu",25,"Female",16900));
+        employees.add(new Employee(1, "Yuto", 23, "Male", 15000));
+        employees.add(new Employee(2, "JC", 22, "Male", 10000));
+        employees.add(new Employee(3, "Khali", 21, "Female", 10000));
+        employees.add(new Employee(4, "Donald", 21, "Male", 104400));
+        employees.add(new Employee(5, "Bob", 69, "Male", 10000));
+        employees.add(new Employee(6, "Zagu", 25, "Female", 16900));
     }
 
     @GetMapping
@@ -33,14 +33,15 @@ public class EmployeesController {
                 .findFirst()
                 .orElse(null);
     }
-    @GetMapping(params="gender")
-    public List<Employee> getAllMaleEmployees(@RequestParam String gender) {
+
+    @GetMapping(params = "gender")
+    public List<Employee> getAllGenderEmployees(@RequestParam String gender) {
         return employees.stream()
                 .filter(employee -> gender.equals(employee.getGender()))
                 .collect(Collectors.toList());
     }
 
-    @GetMapping(params={"pageIndex", "pageSize"})
+    @GetMapping(params = {"pageIndex", "pageSize"})
     public List<Employee> getEmployeesByPagination(@RequestParam Long pageIndex, @RequestParam Long pageSize) {
         return employees.stream()
                 .skip((pageIndex - 1) * pageSize)
