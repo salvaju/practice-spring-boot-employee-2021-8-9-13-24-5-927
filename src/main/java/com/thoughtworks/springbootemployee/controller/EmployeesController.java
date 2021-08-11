47b83dfcb9c6591cd58806bd684571562c0c816c
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-11 16:25:49 +0800

    [employeesPairProgramming][Juan Carlos Salvador] feat: added getEmployeesByPagination API

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -16,6 +16,9 @@ public class EmployeesController {
         employees.add(new Employee(1,"Yuto",23,"Male",15000));
         employees.add(new Employee(2,"JC",22,"Male",10000));
         employees.add(new Employee(3,"Khali",21,"Female",10000));
+        employees.add(new Employee(4,"Donald",21,"Male",104400));
+        employees.add(new Employee(5,"Bob",69,"Male",10000));
+        employees.add(new Employee(6,"Zagu",25,"Female",16900));
     }
 
     @GetMapping
@@ -35,7 +38,14 @@ public class EmployeesController {
         return employees.stream()
                 .filter(employee -> gender.equals(employee.getGender()))
                 .collect(Collectors.toList());
-
-
     }
+
+    @GetMapping(params={"pageIndex", "pageSize"})
+    public List<Employee> getEmployeesByPagination(@RequestParam Long pageIndex, @RequestParam Long pageSize) {
+        return employees.stream()
+                .skip((pageIndex - 1) * pageSize)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+    }
+
 }
