c4d2b78625de2d2065eb873e591de9c39abfdfc6
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-14 11:04:32 +0800

    [Company][Juan Carlos Salvador] refactor: rename method getCompanyByPagination() to getCompaniesByPagination()

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -33,7 +33,7 @@ public class CompanyController {
     }
 
     @GetMapping(params = {"pageIndex", "pageSize"})
-    public List<Company> getCompanyByPagination(@RequestParam Integer pageIndex, @RequestParam Integer pageSize) {
+    public List<Company> getCompaniesByPagination(@RequestParam Integer pageIndex, @RequestParam Integer pageSize) {
         return companyService.getCompaniesByPagination(pageIndex, pageSize);
     }
 
