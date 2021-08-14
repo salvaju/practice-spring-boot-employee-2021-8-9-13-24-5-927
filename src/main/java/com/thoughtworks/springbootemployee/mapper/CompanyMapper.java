083564c69ebcc65a7761ec5f958424d1775ac1b5
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-14 14:26:26 +0800

    [Company][Juan Carlos Salvador] refactor: optimized imports CompanyMapper

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -2,10 +2,7 @@ package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.CompanyRequest;
 import com.thoughtworks.springbootemployee.dto.CompanyResponse;
-import com.thoughtworks.springbootemployee.dto.EmployeeReponse;
-import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
 import com.thoughtworks.springbootemployee.model.Company;
-import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
